82b561bea5406b5a293879de48371171b367712f
chuanjiang <chuanjiang@futunn.com>
2024-02-27 11:49:15 +0800

    添加代码

--- ch1/server3/main.go
+++ ch1/server3/main.go
@@ -17,15 +17,38 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-//!+handler
+// !+handler
 // handler echoes the HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
+	// 打印请求信息到浏览器
 	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	// 打印URL的所有类型到浏览器
+	fmt.Fprintf(w, "Url\n")
+	fmt.Fprintf(w, "%s\n", r.URL)
+	fmt.Fprintf(w, "Url type:%T\n", r.URL)
+	fmt.Fprintf(w, "Url String:%q\n", r.URL.String())
+	fmt.Fprintf(w, "Url.Path:%q\n", r.URL.Path)
+	fmt.Fprintf(w, "Url.Scheme:%q\n", r.URL.Scheme)
+	fmt.Fprintf(w, "Url.RawQuery:%q\n", r.URL.RawQuery)
+	fmt.Fprintf(w, "Url.Fragment:%q\n", r.URL.Fragment)
+	fmt.Fprintf(w, "Url.Host:%q\n", r.URL.Host)
+	fmt.Fprintf(w, "Url.User:%q\n", r.URL.User)
+
+	fmt.Fprintf(w, "Body\n")
+	data := make([]byte, r.ContentLength)
+	r.Body.Read(data)
+	defer r.Body.Close()
+	fmt.Fprintf(w, "Body = %q\n", string(data))
+
+	fmt.Fprintf(w, "Header\n")
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
 	fmt.Fprintf(w, "Host = %q\n", r.Host)
 	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	fmt.Fprintf(w, "Form\n")
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
---
+++ chuantest/chuan.go
@@ -0,0 +1,14 @@
+package chuantest
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(test123())
+
+}
+
+func test123() (abc int) {
+	fmt.Println("test123")
+	return
+}
